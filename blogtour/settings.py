e6b49c336bfadf2a40cca8799424d91ffc1c742d
vUSYK <vUSYK@proton.me>
2024-09-30 22:42:44 +0300

    Set DEBUG to False and update ALLOWED_HOSTS for production

--- blogtour/settings.py
+++ blogtour/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '$lv^$h6t%ed)wplt&^-+^9^fq7701uu9a@pm87f_fk)hy50#h2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [freetourua.pythonanywhere.com]
 # Application definition
 
 INSTALLED_APPS = [
